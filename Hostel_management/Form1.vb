fa8219d6e865d6ec34f49c084e9caa48331ef7a1
antoh55 <anthonyvigetty@gmail.com>
2022-01-01 17:13:08 +0300

    Add project files.

---
+++ Hostel_management/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim connection As New OleDb.OLEDBCONNECTION("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Dell-PC\Desktop\Mwende2\Hostel_management\test_db.mdb")
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+End Class
